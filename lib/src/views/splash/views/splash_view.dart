ec6bfc4e3f5b24b4985ca88bbe3cdae171a0d9ab
TechnourceDeveloper <info@technource.in>
2023-07-20 15:17:16 +0530

    code

--- lib/src/views/splash/views/splash_view.dart
+++ lib/src/views/splash/views/splash_view.dart
@@ -1,44 +1,41 @@
-/*
- * Copyright (c) 2023 Technource. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *  Email: support@technource.com
- *  Developed by Technource (https://www.technource.com)
- */
-
 import 'package:flutter/material.dart';
-import 'package:flutter_setup/global/constant/resources/resources.dart';
-import 'package:flutter_setup/global/dialog/dialog_helper.dart';
-import 'package:flutter_setup/global/utils/config.dart';
 import 'package:get/get.dart';
+import '../../../../global/constant/resources/resources.dart';
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
-  const SplashView({Key? key}) : super(key: key);
+  final SplashController splashController = Get.put(SplashController());
+
+  SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: R.colors.kcPrimaryColor,
-        body: Center(
-            child: Text(Config.appName,
-                style: R.styles.txt24sizeW600kcCaptionLightGray)));
+        body: SafeArea(
+            child: Stack(children: [
+          splashScreenBackground(),
+          splashScreenCenterComponent(),
+        ])));
+  }
+
+  splashScreenBackground() {
+    return SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Image.asset(R.assets.welcomeBg4x, fit: BoxFit.fill));
+  }
+
+  splashScreenCenterComponent() {
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+          Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset(R.assets.appLogo,
+              ))
+        ]));
   }
 }
